092bc3803a0083ba6a42b0c31307fd6870388c2d
Dan Lenski <dlenski@gmail.com>
2020-03-12 15:29:04 -0700

    test targets: remove dead one, less noisy format

--- tests/test_sniffers.py
+++ tests/test_sniffers.py
@@ -11,8 +11,32 @@ from what_vpn.sniffers import Hit, sniffers
 session = SnifferSession()
 session.timeout = 10
 
-matched_vpns = ['vpn.{}.edu'.format(d) for d in ('drew','syr','andrews','northeastern','yale','fau','uca','nwciowa','simmons','nl','ycp','smcvt','louisville','usmma')] + ['cpvpn.its.hawaii.edu']
-unmatched_vpns = ['vpn.{}.edu'.format(d) for d in ('acu','aurora','brown','wisc','whitworth','valpo','uu')]
+matched_vpns = ['vpn.{}.edu'.format(d) for d in (
+    'drew',
+    'syr',
+    'andrews',
+    'northeastern',
+    'yale',
+    'fau',
+    'uca',
+    'simmons',
+    'nl',
+    'ycp',
+    'smcvt',
+    'louisville',
+    'usmma',
+    )] + [
+    'cpvpn.its.hawaii.edu',
+    ]
+unmatched_vpns = ['vpn.{}.edu'.format(d) for d in (
+    'acu',
+    'aurora',
+    'brown',
+    'wisc',
+    'whitworth',
+    'valpo',
+    'uu',
+    )]
 
 def _count_hits(server):
     hits = 0
