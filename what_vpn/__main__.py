8382ea7feb13f120dc1c7db89d542774086dca54
Daniel Lenski <dlenski@gmail.com>
2021-02-21 23:39:15 -0800

    don't keep trying after non-application-layer SSL/timeout exceptions

--- what_vpn/__main__.py
+++ what_vpn/__main__.py
@@ -21,6 +21,7 @@ from sys import stderr, stdout
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('-k','--keep-going', action='store_true', help='Keep going after first hit')
+    p.add_argument('-K','--keep-going-after-exception', action='store_true', help='Keep going after an SSL or timeout exception')
     p.add_argument('-t','--timeout', metavar='SEC', type=lambda x: int(x) or None, default=10, help='Timeout in seconds (default %(default)s, 0 for none)')
     x = p.add_mutually_exclusive_group()
     x.add_argument('-v','--verbose', default=0, action='count')
@@ -78,7 +79,7 @@ def main():
                 continue
 
         hits = []
-        ssle = timeout = 0
+        ssle = timeout = bail = tried = 0
         for sniffer in sniffers:
             if args.specific and sniffer.__name__ not in args.specific:
                 continue
@@ -93,9 +94,11 @@ def main():
             except rex.Timeout as e:
                 ex = 'timeout'
                 timeout += 1
+                bail += 1
             except rex.SSLError as e:
                 ex = 'SSL error'
                 ssle += 1
+                bail += 1
             except rex.ConnectionError as e:
                 ex = 'connection error'
             except Exception as e:
@@ -104,6 +107,7 @@ def main():
                 ex = e.__class__.__name__
             else:
                 ex = 'no match'
+            tried += 1
 
             if hit:
                 if args.csv:
@@ -115,16 +119,19 @@ def main():
 
             if hit and not args.keep_going:
                 break
+            if bail and not args.keep_going_after_exception:
+                break
 
         if args.verbose:
             print("  => ", end='')
 
+        extra = ' (tried %d/%d sniffers)' % (tried, len(sniffers)) if tried < len(sniffers) else ''
         if ssle:
-            errs = 'SSL errors'
+            errs = 'SSL errors'+extra
         elif timeout:
-            errs = 'timeout'
+            errs = 'timeout'+extra
         elif not hits:
-            errs = 'no match'
+            errs = 'no match'+extra
 
         if args.csv and not hits:
             wr.writerow((server, errs))
