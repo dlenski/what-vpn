629345c3ffeffbdb78214a2251972a262abd896c
Dimitri Papadopoulos Orfanos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2022-06-09 22:41:26 +0200

    update list of test targets (#18)

--- tests/test_sniffers.py
+++ tests/test_sniffers.py
@@ -29,6 +29,7 @@ matched_vpns = [('vpn.{}.edu'.format(d), s) for d, s in (
     )] + [
     ('securesso.aurora.edu', sn.global_protect),
     ('vpn.physics.ox.ac.uk', sn.sstp),
+    ('uwmadison.vpn.wisc.edu', sn.global_protect),
     ('umsovpn.umassp.edu', sn.check_point),
     ('gmhssl.gmha.org', sn.barracuda),
     ('sslvpn.co.adams.il.us', sn.barracuda),
@@ -47,7 +48,6 @@ matched_vpns = [('vpn.{}.edu'.format(d), s) for d, s in (
     ]
 
 unmatched_vpns = ['vpn.{}.edu'.format(d) for d in (
-    'wisc',
     'valpo',
     'uu',
     'drew',  # FIXME: false-negative SonicWall
